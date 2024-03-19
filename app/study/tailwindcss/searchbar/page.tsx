455b994232f9499f2ab649f63106647ad4dd177c
clean-teach <82921898+clean-teach@users.noreply.github.com>
2024-03-20 01:47:20 +0900

    #3.5 Responsive Modifiers
    ## 목표
    
    - **Modifiers** 을 사용하여, 반응형 디자인을 구현하는 방법에 대해서 알아본다.
        - 이전 강의에서 만든 “검색바 컴포넌트”를 반응형으로 스타일링 하여 본다.
    
    ## 정리
    
    [Responsive Design - Tailwind CSS](https://tailwindcss.com/docs/responsive-design)
    
    - Tailwind CSS 는 모바일 우선이다.
        - 기본적으로 스타일링 한 것은 모바일부터 적용되고,
        - **Modifiers** 을 사용하여, 더 큰 크기의 화면을 설정하는 식이다.
            - **Modifiers** 가 제공하는 screen 값은 최소값(`min`) 을 의미한다.
    - Tailwind CSS 를 사용하면,
        - 미디어쿼리를 따로 작성 할 필요 없이, 클래스에 간단한 키워드를 추가하여 정돈 된 반응형 디자인을 구현 할 수 있다.
    - Tailwind CSS를 잘 못 입력하였을 경우, [지난 강의](https://www.notion.so/3-2024-UPDATE-TAILWIND-1d33a42bb752439bbe20692124b210af?pvs=21)에서 설치한 확장 프로그램인 IntelliSense 가 실시간으로 에러를 알려준다.

--- app/study/tailwindcss/searchbar/page.tsx
+++ app/study/tailwindcss/searchbar/page.tsx
@@ -1,9 +1,9 @@
 export default function SearchBar() {
     return (
-        <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
-            <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col gap-2">
+        <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+            <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col md:flex-row gap-2">
                 <input type="text" placeholder="Search here..." className="w-full rounded-full h-10 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-4 focus:ring-1 focus:ring-offset-gray-400 transition-shadow placeholder:drop-shadow" />
-                <button className="bg-black text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform outline-none">Search</button>
+                <button className="bg-black text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform outline-none md:px-10">Search</button>
             </div>
         </main>
     );

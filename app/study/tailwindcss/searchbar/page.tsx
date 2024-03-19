b78b86a48e6d1a5e0ba23864d0b085fd89f48519
clean-teach <82921898+clean-teach@users.noreply.github.com>
2024-03-20 02:47:08 +0900

    #3.7 State Modifiers
    ## 목표
    
    - 자식 요소 선택자에 대해서 알아본다.
    - has 선택자에 대해서 알아본다.
    
    ## 정리
    
    ### `*:`
    
    [Handling Hover, Focus, and Other States - Tailwind CSS](https://tailwindcss.com/docs/hover-focus-and-other-states#styling-direct-children)
    
    ### `has-[]:`
    
    - 부모요소에서 사용하는 선택자이며, 자식요소 중 원하는 class명이나 상태(`:invalid` 등)를 갖고 있는지 여부를 검사 한다.
    
    [Handling Hover, Focus, and Other States - Tailwind CSS](https://tailwindcss.com/docs/hover-focus-and-other-states#styling-based-on-descendants)

--- app/study/tailwindcss/searchbar/page.tsx
+++ app/study/tailwindcss/searchbar/page.tsx
@@ -1,10 +1,10 @@
 export default function SearchBar() {
     return (
         <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-            <div className="bg-gradient-to-tr from-cyan-400 to-green-400 w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col md:flex-row gap-2">
-                <input type="email" placeholder="Email address" required className="w-full rounded-full h-10 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-4 focus:ring-1 focus:ring-offset-gray-50 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer" />
+            <div className="bg-gradient-to-tr from-cyan-100 to-blue-100 w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none ring ring-transparent transition-shadow has-[:invalid]:ring-red-200">
+                <input type="text" placeholder="Email address" required className="w-full rounded-full h-10 bg-white pl-5 ring ring-transparent focus:ring-green-500 focus:ring-offset-4 focus:ring-1 focus:ring-offset-gray-50 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer" />
                 <span className="text-red-500 font-medium hidden peer-invalid:block">Email is required</span>
-                <button className="bg-green-500 text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform outline-none md:px-10 min-w-36 peer-invalid:bg-red-500 peer-required:bg-green-500">Log in</button>
+                <button className="bg-green-500 text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform md:px-10 min-w-36 peer-invalid:bg-red-500 peer-required:bg-green-500">Log in</button>
             </div>
         </main>
     );

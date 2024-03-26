7b7244af7f168c4550c38b86573c88a3fa9ce243
clean-teach <82921898+clean-teach@users.noreply.github.com>
2024-03-26 23:51:25 +0900

    #3.9 Group Modifiers
    ## 목표
    
    - Group Modifiers 에 대하여 알아본다.
        - Group Modifiers 가 제공하는 여러 속성에 대하여 알아보고,
        - 이를 활용하여 기존에 자바스크립트로만 구현 가능 할 것 같았던 기능을 구현하여 본다.
            - 컨테이너 안의 input 요소가 포커스 되었을 때,
    
                → 해당 컨테이너에 포함된 다른 요소를 컨트롤 하여 본다.
    
    ## 정리
    
    [Handling Hover, Focus, and Other States - Tailwind CSS](https://tailwindcss.com/docs/hover-focus-and-other-states#styling-based-on-parent-state)
    
    - Group Modifiers 는 부모 요소의 State에 따라서 자식 요소의 Style을 변경하는 것을 도와준다.
    - [이전에 배운 has 선택자](https://www.notion.so/3-2024-UPDATE-TAILWIND-1d33a42bb752439bbe20692124b210af?pvs=21)와는 반대로 부모의 State에 따라서 자식을 변경한다.
        - has 선택자는 자식의 State에 따라서 부모를 변경한다.

--- app/study/tailwindcss/list/page.tsx
+++ app/study/tailwindcss/list/page.tsx
@@ -1,11 +1,11 @@
 export default function ListComponent() {
     return (
-        <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5 flex-col gap-4">
+        <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-10 flex-col gap-4">
             <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col gap-4">
                 {["Nico", "Me", "You", "YourSelf", ""].map((person, index) => (
-                    <div key={index} className="flex items-center gap-3 p-2.5 rounded-xl even:bg-cyan-100 odd:bg-gray-100 border-b-2 last:border-b-0">
+                    <div key={index} className="flex items-center gap-3 p-2.5 rounded-xl even:bg-cyan-100 odd:bg-gray-100 border-b-2 last:border-b-0 group">
                         <div className="size-10 bg-blue-400 rounded-full" />
-                        <span className="text-lg font-medium empty:w-40  empty:h-5 empty:rounded-full empty:bg-gray-400 empty:animate-pulse">{person}</span>
+                        <span className="text-lg font-medium empty:w-40  empty:h-5 empty:rounded-full empty:bg-gray-400 empty:animate-pulse group-hover:text-red-500">{person}</span>
                         <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full animate-bounce relative">
                             <span>{index}</span>
                             <div className="size-6 bg-red-500 rounded-full absolute animate-ping" />
@@ -23,6 +23,12 @@ export default function ListComponent() {
                         <div className="h-4 w-20 rounded-full bg-gray-400"></div>
                     </div>
                 ))}
+            </div>
+            <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col gap-4 group">
+                <h2>자식 요소 focus 시, 부모요소에 포함된 다른 자식 요소 Styling</h2>
+                <input type="text" placeholder="Write your Email" className="bg-gray-100 w-full" />
+                <span className="group-focus-within:block hidden">Make Sure it is a valid email...</span>
+                <button>Submit</button>
             </div>
         </main>
     );

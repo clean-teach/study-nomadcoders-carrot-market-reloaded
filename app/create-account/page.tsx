f94b93029472fecc4d702fa9ff7be7be5ebd2258
clean-teach <82921898+clean-teach@users.noreply.github.com>
2024-03-29 01:35:53 +0900

    #4.2 Form Components
    ## 목표
    
    - 이전 강의에서 만든 요소들을 각각의 React 컴포넌트로 추상화 한다.
        - 각 컴포넌트들은 Props로 커스텀 할 수 있게 만들 것이다.
    - form 요소의 기능을 실제로 다루는 방법에 대해서는 추후에 배워본다.
    
    ## 참고
    
    ### type 과 interface의 차이점
    
    [Documentation - Everyday Types](https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#differences-between-type-aliases-and-interfaces)
    
    → 타입스크립트 공식문서에 의하면, `type` 별칭을 사용하여야 할 특별한 이유가 없을 때에는 `interface`를 사용할 것을 권장한다.

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,3 +1,5 @@
+import FormInput from "@/components/form-Input";
+import FormButton from "@/components/form-btn";
 import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 
@@ -9,11 +11,11 @@ export default function CreateAccount() {
                 <h2 className="text-xl">Fill in the form below to join!</h2>
             </div>
             <form className="flex flex-col gap-3">
-                <div>
-                    <input type="text" placeholder="Username" required className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-1 focus:ring-2 ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400" />
-                    <span className="text-red-500 font-medium">Input Error</span>
-                </div>
-                <button className="primary-btn h-10">Create Account</button>
+                <FormInput type="text" placeholder="UserName" required={true} errors={['Username is too short']} />
+                <FormInput type="email" placeholder="Email" required={true} errors={[]} />
+                <FormInput type="password" placeholder="Password" required={true} errors={[]} />
+                <FormInput type="password" placeholder="Confirm Password" required={true} errors={[]} />
+                <FormButton text="Create Account" loading={true} />
             </form>
             <div className="w-full h-px bg-neutral-500" />
             <div>

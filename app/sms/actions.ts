1ce80f6857571e1eb82e3d47af7ed1329a663067
clean-teach <82921898+clean-teach@users.noreply.github.com>
2024-04-11 00:08:17 +0900

    #6.8 SMS Validation
    ## 목표
    
    - SMS 로그인의 검증 과정을 마무리 하여 본다.
        - `actions.ts` 함수의 `prevState` 를 본격적으로 사용하여 본다.
        - 단계에 따라서 다이네믹 한 인터렉티브 Form을 만들어 본다.
            1. 첫 번째 단계에서는 전화번호 input만을 노출하고 Token input은 숨긴다.
            2. 전화번호가 전달되면, 토큰 input을 제공한다.
        - 각 input 검증과정에서 발생하는 에러를 화면에 표시해 본다.
    
    ## 정리
    
    - `actions.ts` 함수의 `prevState` 를 사용하여, 여러 번의 form 제출에 따른 다이네믹 한 form을 구현해 보았다.
        - 타입스크립트 환경이기 때문에 타입을 잘 정의하면, `actions.ts` 함수가 반환하는 경우의 수를 알아서 추론해준다.

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,14 +1,51 @@
 "use server";
 
+import { redirect } from "next/navigation";
 import validator from "validator";
 import { z } from "zod";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone) => validator.isMobilePhone(phone, "ko-KR"),
+    "Wrong phone format"
+  );
 
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
+interface ActionState {
+  token: boolean;
+}
+
 // const smsLogIn = async () => {} // 표현식 방법 (아래 선언식 방법과 같은 의미이다.)
-export async function smsLogIn(prevState: any, formData: FormData) {
-  console.log(typeof formData.get("token"));
-  console.log(typeof tokenSchema.parse(formData.get("token")));
+export async function smsLogIn(prevState: ActionState, formData: FormData) {
+  const phone = formData.get("phone");
+  const token = formData.get("token");
+
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+
+    if (!result.success) {
+      return {
+        token: false,
+        error: result.error.flatten(),
+      };
+    } else {
+      return {
+        token: true,
+      };
+    }
+  } else {
+    const result = tokenSchema.safeParse(token);
+
+    if (!result.success) {
+      return {
+        token: true,
+        error: result.error.flatten(),
+      };
+    } else {
+      redirect("/");
+    }
+  }
 }

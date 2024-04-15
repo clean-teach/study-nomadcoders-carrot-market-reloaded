ca24ee0257c646303f7088163272c25caeb469ad
clean-teach <82921898+clean-teach@users.noreply.github.com>
2024-04-16 00:25:47 +0900

    #8.2 Password Hashing
    ## 목표
    
    1. 입력 받은 비밀번호를 해싱하고
    2. 이 변환 된 해시 코드를 DB에 저장한다.
    
        → 만약에 DB가 해킹 된다고 하더라도, 해시 코드만 가지고는 기존에 입력 된 비밀번호를 알 수가 없다.
    
    ## 해싱(Hashing)
    
    - 비밀번호를 변환하여 암호화 하는 것
    
    ### 해시 함수
    
    - 비밀번호를 입력 받아서, 무작위로 보이는 문자열 출력
    - 단방향
        - 똑같은 비밀번호로는 똑같은 해시 문자열을 출력한다.
        - 반대 방향으로, 이미 해시화 된 문자열을 입력한다고 기존에 입력된 비밀번호를 얻을 수는 없다.
    
    ## `bcrypt`
    
    [npm: bcrypt](https://www.npmjs.com/package/bcrypt)
    
    - 비밀번호를 해시하는 데 도움이 되는 라이브러리입니다.
        - 해싱 알고리즘 반복 횟수를 설정 할 수 있어서, 하나의 입력문을 여러번 해싱 할 수 있다.
    - `npm i bcrypt`
    - `npm i @types/bcrypt`
    
    ## 참고
    
    [비밀번호 털렸다고?  암호화. 해시함수. 5분 설명.](https://www.youtube.com/watch?v=67UwxR3ts2E)
    
    [How To Safely Store A Password](https://codahale.com/how-to-safely-store-a-password/)

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -7,6 +7,7 @@ import {
 } from "@/lib/constants";
 import db from "@/lib/db";
 import { z } from "zod";
+import bcrypt from "bcrypt";
 
 const checkUniqueUsername = async (username: string) => {
   const user = await db.user.findUnique({
@@ -88,8 +89,18 @@ export async function createAccount(preveState: any, formData: FormData) {
   if (!result.success) {
     return result.error.flatten();
   } else {
-    // hash password
-    // save the user to db
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
+    console.log(user);
     // log the user in
     // redirect "/home"
   }
